ac948f8305ce100a2af611483e8fb4f4a7d91a41
Minivera <gstpierre01@gmail.com>
2021-11-20 17:53:41 -0500

    Add deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import { ViteAliases } from 'vite-aliases';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/dart600-creative-project/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/DART600-project/' : '',
   plugins: [reactRefresh({
     exclude: /__generated__/,
   }), ViteAliases()],
